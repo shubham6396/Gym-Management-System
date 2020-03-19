d1897d675446e675f5183aea57c5c399cf635dae
mihir <mihirmv477@gmail.com>
2020-03-19 17:27:29 -0400

    Fixed - Session now Persists on Refresh

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -34,7 +34,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return({
-            onTryAutoLogin: () => dispatch(actions.authCheckState)
+            onTryAutoLogin: () => dispatch(actions.authCheckState())
         }
     )
 }
